271272f5a3e4db95fcfd2fe362aa8c69ce56b7b1
karanbir-singh <karans1723@gmail.com>
2021-03-02 21:33:53 +0100

    Added react-bootstrap and Navbar on page

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css";
+import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap";
 
 // App's root element
 const rootElement = document.getElementById("root");
 
 function App() {
     return (
-        <p>React.js is working</p>
+        <>
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-primary">Search</Button>
+                </Form>
+            </Navbar>
+        </>
     );
 }
 
