18b7a053b1b75d08d528e3d45ca0a8c12b5958d3
karanbir-singh <karans1723@gmail.com>
2021-03-03 18:44:27 +0100

    Added play button, skip next and previous buttons

---
+++ react-frontend/src/components/BsNavbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles.css";
+import assets from "../assets/*.png";
+import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap";
+
+export function BsNavbar() {
+    return (
+        <Navbar bg="light" variant="light">
+            <Navbar.Brand href="../../public/index.html"><img src={assets.logo} width="30px" height="30px"></img></Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="../../public/index.html">Home</Nav.Link>
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-primary">Search</Button>
+            </Form>
+        </Navbar>
+    );
+}
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css";
-import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap";
+import { BsNavbar } from "./components/BsNavbar";
+import { Track } from "./components/Track";
+import { Row } from "react-bootstrap";
 
 // App's root element
 const rootElement = document.getElementById("root");
@@ -10,18 +12,22 @@ const rootElement = document.getElementById("root");
 function App() {
     return (
         <>
-            <Navbar bg="light" variant="light">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-primary">Search</Button>
-                </Form>
-            </Navbar>
+            <BsNavbar />
+            <Row xs={1} sm={2} md={3} lg={3} xl={6} >
+                <Track />
+                <Track />
+                <Track />
+                <Track />
+                <Track />
+                <Track />
+
+                <Track />
+                <Track />
+                <Track />
+                <Track />
+                <Track />
+                <Track />
+            </Row>
         </>
     );
 }
