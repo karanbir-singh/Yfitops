ba5b83b48e69882fdebecb16b4b040d561157610
karanbir-singh <karans1723@gmail.com>
2021-03-22 19:18:57 +0100

    Add some changes

--- react-frontend/src/components/ReactSideNav.js
+++ react-frontend/src/components/ReactSideNav.js
@@ -11,7 +11,7 @@ const user = require('../user.js');
 export function ReactSidenav(props) {
     //> States and Context
     const { state, dispatch } = useContext(AppContext);
-    const [recentPlayed, setRecentPlayed] = useState([]);
+    const [recentPlayed, setRecentPlayed] = useLocalStorage('recent-played',[]);
     const [isUploading, setIsUploading] = useState(false);
 
     //> Formats the track title
@@ -57,6 +57,43 @@ export function ReactSidenav(props) {
         })).includes(true);
     }
 
+    //> Hook
+    function useLocalStorage(key, initialValue) {
+        // State to store our value
+        // Pass initial state function to useState so logic is only executed once
+        const [storedValue, setStoredValue] = useState(() => {
+            try {
+                // Get from local storage by key
+                const item = window.localStorage.getItem(key);
+                // Parse stored json or if none return initialValue
+                return item ? JSON.parse(item) : initialValue;
+            } catch (error) {
+                // If error also return initialValue
+                console.log(error);
+                return initialValue;
+            }
+        });
+
+        // Return a wrapped version of useState's setter function that ...
+        // ... persists the new value to localStorage.
+        const setValue = value => {
+            try {
+                // Allow value to be a function so we have same API as useState
+                const valueToStore =
+                    value instanceof Function ? value(storedValue) : value;
+                // Save state
+                setStoredValue(valueToStore);
+                // Save to local storage
+                window.localStorage.setItem(key, JSON.stringify(valueToStore));
+            } catch (error) {
+                // A more advanced implementation would handle the error case
+                console.log(error);
+            }
+        };
+
+        return [storedValue, setValue];
+    }
+
     //> On "state.index" changed...
     useEffect(() => {
         // Get played track
@@ -65,7 +102,11 @@ export function ReactSidenav(props) {
         // Check if "recentPlayed" exceeded 7 as max size 
         // or "state.index" is -1
         // or "recentPlayed" already contains "playedTrack" 
-        if (state.index !== -1 && recentPlayed.length < 8 && !checkDuplicates(playedTrack)) {
+        if (state.index !== -1 && !checkDuplicates(playedTrack)) {
+            if(recentPlayed.length === 16){
+                recentPlayed.pop()
+            }
+
             // If not, update "recentPlayed"
             let updatedRecentPlayed = [playedTrack, ...recentPlayed];
             setRecentPlayed(updatedRecentPlayed);
